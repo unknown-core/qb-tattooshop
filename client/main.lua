c8f38cca33770136d4f8e3d81ff34999db006e45
AmirKasra_MomenKashani <71598934+AmirKasraKashani@users.noreply.github.com>
2022-05-28 10:06:16 +0430

    fix naked
    fix naked

--- client/main.lua
+++ client/main.lua
@@ -94,7 +94,7 @@ function GetNaked()
                 ["mask"]         = { item = 0, texture = 0},  -- Masker
             },
         }
-        TriggerEvent('qb-clothes:client:loadOutfit'', nakedMale)
+        TriggerEvent('qb-clothes:client:loadOutfit', nakedMale)
     else
         local nakedFemale = {
             outfitData = {
